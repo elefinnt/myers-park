9efb1dc8fb99766d119809801e6dc3a657301878
Finn Hackshaw <hackshawf5@gmail.com>
2023-11-09 14:21:03 +1300

    feat: header ui

---
+++ components/CustomHeader.tsx
@@ -0,0 +1,130 @@
+import {
+  View,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  TextInput,
+} from "react-native";
+import React from "react";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import { Ionicons } from "@expo/vector-icons";
+import Colors from "@/constants/Colors";
+import { Link } from "expo-router";
+
+const SearchBar = () => (
+  <View style={styles.searchContainer}>
+    <View style={styles.searchSection}>
+      <View style={styles.searchField}>
+        <Ionicons
+          style={styles.searchIcon}
+          name="ios-search"
+          size={20}
+          color={Colors.medium}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Search"
+          placeholderTextColor={Colors.medium}
+        />
+      </View>
+      <Link href={"/"} asChild />
+      <TouchableOpacity style={styles.optionButton}>
+        <Ionicons name="options-outline" size={20} />
+      </TouchableOpacity>
+    </View>
+  </View>
+);
+
+const CustomHeader = () => {
+  return (
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+        <TouchableOpacity onPress={() => {}}>
+          <Icon name="weight-lifter" style={styles.weightLifter} />
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.titleContainer}>
+          <Text style={styles.title}>Home</Text>
+          <View style={styles.userTitle}>
+            <Text style={styles.subtitle}>Welcome User!</Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.profileButton}>
+          <Ionicons name="person-outline" size={20} color={Colors.primary} />
+        </TouchableOpacity>
+      </View>
+      <SearchBar />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: "#FFF",
+  },
+  container: {
+    height: 60,
+    backgroundColor: "#FFF",
+    flexDirection: "row",
+    gap: 20,
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingHorizontal: 20,
+  },
+  weightLifter: {
+    fontSize: 30,
+  },
+  titleContainer: {
+    flex: 1,
+  },
+  title: {
+    fontSize: 14,
+    color: Colors.medium,
+  },
+  subtitle: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  userTitle: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  profileButton: {
+    backgroundColor: Colors.lightGrey,
+    padding: 10,
+    borderRadius: 50,
+  },
+  searchContainer: {
+    height: 60,
+    backgroundColor: "#fff",
+  },
+  searchSection: {
+    flexDirection: "row",
+    gap: 10,
+    flex: 1,
+    paddingHorizontal: 20,
+    alignItems: "center",
+  },
+  searchField: {
+    flex: 1,
+    backgroundColor: Colors.lightGrey,
+    borderRadius: 8,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  optionButton: {
+    padding: 10,
+    borderRadius: 50,
+  },
+  input: {
+    padding: 10,
+  },
+  searchIcon: {
+    paddingLeft: 10,
+  },
+});
+
+export default CustomHeader;
