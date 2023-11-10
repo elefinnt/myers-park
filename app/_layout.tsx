2d5a452ec362fd5264184708341d85d242fc4fed
Finn Hackshaw <hackshawf5@gmail.com>
2023-11-10 17:36:56 +1300

    init: modal animation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,17 @@
-import { Stack } from "expo-router";
+import { Stack, useNavigation } from "expo-router";
 import CustomHeader from "@/components/CustomHeader";
 import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
+import Colors from "@/constants/Colors";
+import { Ionicons } from "@expo/vector-icons";
+import { TouchableOpacity } from "react-native-gesture-handler";
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
   initialRouteName: "index",
 };
 
 export default function RootLayoutNav() {
+  const navigation = useNavigation();
+
   return (
     <BottomSheetModalProvider>
       <Stack>
@@ -14,6 +19,30 @@ export default function RootLayoutNav() {
           name="index"
           options={{
             header: () => <CustomHeader />,
+          }}
+        />
+        <Stack.Screen
+          name="(modal)/filter"
+          options={{
+            presentation: "modal",
+            headerTitle: "Filter",
+            headerShadowVisible: false,
+            headerStyle: {
+              backgroundColor: Colors.lightGrey,
+            },
+            headerLeft: () => (
+              <TouchableOpacity
+                onPress={() => {
+                  navigation.goBack();
+                }}
+              >
+                <Ionicons
+                  name="close-outline"
+                  size={28}
+                  color={Colors.cubBlack}
+                />
+              </TouchableOpacity>
+            ),
           }}
         />
       </Stack>
